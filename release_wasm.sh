20ac74d4dc48f1bbbc6a1f0049d41f2ac434a2cc
sifmuna00 <ntthang3141@gmail.com>
2024-12-02 01:45:30 +0700

    e

--- release_wasm.sh
+++ release_wasm.sh
@@ -3,4 +3,5 @@
 cur=$(basename "$PWD")
 
 cargo build --release --target wasm32-unknown-unknown && \
-cp target/wasm32-unknown-unknown/release/$cur.wasm .
+cp target/wasm32-unknown-unknown/release/$cur.wasm . && \
+tar -cf $cur-wasm.zip index.html $cur.wasm assets/*
